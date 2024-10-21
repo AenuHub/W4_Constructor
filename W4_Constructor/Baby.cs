ffdeea31505ed9136a5b33a68f870eb1b7b8f67f
AenuHub <jesuskane92@gmail.com>
2024-10-21 14:02:36 +0300

    Add project files.

---
+++ W4_Constructor/Baby.cs
@@ -0,0 +1,68 @@
+﻿namespace W4_Constructor
+{
+    public class Baby
+    {
+        private string _firstName;
+        private string _lastName;
+        private DateTime _birthDate;
+
+        // getters and setters
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public DateTime BirthDate
+        {
+            get
+            {
+                return _birthDate;
+            }
+            set
+            {
+                _birthDate = value;
+            }
+        }
+
+        // empty constructor
+        public Baby()
+        {
+            Console.WriteLine("Ingaaaa");
+            _birthDate = DateTime.Now;
+        }
+
+        // constructor with parameters
+        public Baby(string firstName, string lastName)
+        {
+            Console.WriteLine("Ingaaaa");
+            _firstName = firstName;
+            _lastName = lastName;
+            _birthDate = DateTime.Now;
+        }
+
+        // introduce method
+        public void Introduce()
+        {
+            Console.WriteLine("Name of the baby is {0} {1} and he/she was born on {2}", _firstName, _lastName, _birthDate);
+        }
+    }
+}
---
+++ W4_Constructor/Program.cs
@@ -0,0 +1,16 @@
+﻿using W4_Constructor;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // create two different baby objects with different constructor calls
+        Baby baby1 = new Baby();
+        baby1.FirstName = "Bradley";
+        baby1.LastName = "Cooper";
+        baby1.Introduce();
+
+        Baby baby2 = new Baby("Alice", "Smith");
+        baby2.Introduce();
+    }
+}
